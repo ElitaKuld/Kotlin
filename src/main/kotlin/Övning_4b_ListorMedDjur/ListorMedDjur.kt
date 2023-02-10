ee3abeef87a5403a81348b3fe87b7a93324a9199
ElitaKuld <elitakuld@gmail.com>
2023-02-10 21:26:59 +0100

    Nya övningar i Kotlin

--- src/main/kotlin/Övning_4b_ListorMedDjur/ListorMedDjur.kt
+++ src/main/kotlin/Övning_4b_ListorMedDjur/ListorMedDjur.kt
@@ -1,5 +1,24 @@
 package Övning_4b_ListorMedDjur
 
+interface Printable {
+    fun getData()
+}
+
+class Animal(private var type: String, private val numberOfLegs: Int) : Printable {
+    override fun getData() = println("Djurets sort: $type, antal ben: $numberOfLegs")
+
+}
+
+fun main() {
+    val cat = Animal("Cat", 4)
+    val human = Animal("Human", 2)
+    val threeLeggedDog = Animal("Dog", 3)
+
+    val animalList = listOf(cat, human, threeLeggedDog)
+
+    animalList.forEach { animal -> animal.getData() }
+}
+
 /*
 Modifiera ditt huvudprogram så att du:
 • Skapa upp ett antal djur och lägg i en lista
